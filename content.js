ceea3d2d5683810db54171bf93785030d65cb805
SimonGodefroid <simon.godefroid@gmail.com>
2019-02-04 15:31:04 +0100

    Delete content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-// chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-//   if (msg.ready === "ready") {
-//     if (confirm('Do you want to have a capture of this screen?')) {
-//       sendResponse({
-//         chrome.downloads.download: "download"
-//       });
-//     }
-//   }
-// });
